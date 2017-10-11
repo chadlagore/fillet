7a6ab0294cdcd16968d4ed03ca00fbdbd467e360
Jordan Schalm <jordan.schalm@gmail.com>
2017-10-10 22:42:57 -0700

    Hook up redux store to app and connect event list to store so it can interact with events reducer

--- src/views/EventList.js
+++ src/views/EventList.js
@@ -6,17 +6,32 @@ import {
     Text,
     View
 } from 'react-native';
+import { connect } from 'react-redux';
+import { addEvents, clearEvents } from './../actions/events';
 
-export default class EventList extends Component {
+class EventList extends Component {
     render () {
         const { navigate } = this.props.navigation;
+        const { events, addEvents, clearEvents } = this.props;
+
         return (
             <View style={styles.container}>
                 <Text>List of events</Text>
                 <Button onPress={() => navigate('EventFilter')} title="Event Filter" />
                 <Button onPress={() => navigate('EventDetail')} title="Event Detail" />
+                <Button onPress={() => addEvents()} title="Add events" />
+                <Button onPress={() => clearEvents()} title="Clear events" />
+                {this._renderEvents(events)}
             </View>
         );
+    }
+
+    _renderEvents (events) {
+        return (
+            <View style={styles.eventsContainer}>
+                {events.map((ev, i) => <Text key={i}>{ev}</Text>)}
+            </View>
+        )
     }
 }
 
@@ -27,12 +42,35 @@ EventList.navigationOptions = {
 EventList.propTypes = {
     navigation: PropTypes.shape({
         navigate: PropTypes.func
-    })
+    }),
+    events: PropTypes.array,
+    addEvents: PropTypes.func,
+    clearEvents: PropTypes.func
 }
+
+// NOTE: this is just for verifying redux is working happily
+const mapDispatchToProps = dispatch => ({
+    addEvents: () => dispatch(addEvents(['a', 'b', 'c'])),
+    clearEvents: () => dispatch(clearEvents())
+});
+
+// Pull events from redux into props so we can display it
+const mapStateToProps = state => ({
+    events: state.events.events
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(EventList);
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff'
+    },
+    eventsContainer: {
+        flex: 1,
+        flexDirection: 'column'
     }
 });
