b1bf2f999240c294fa63b06533420952f28b74dd
Jordan Schalm <jordan.schalm@gmail.com>
2017-11-05 18:21:27 -0800

    remove unused import

--- src/__test__/views/Home.test.js
+++ src/__test__/views/Home.test.js
@@ -1,7 +1,6 @@
 import 'react-native';
 import React from 'react';
 import { Home } from './../../views/Home';
-import MockEvent from './../../mocks/event';
 import renderer from 'react-test-renderer';
 
 test('should render', () => {
